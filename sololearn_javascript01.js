6f3829e56bbd6260e6a742141e7159a44a443b2d
moritama <kopakas@gmail.com>
2019-05-07 18:40:27 +0900

    Update sololearn_javascript01.js

--- sololearn_javascript01.js
+++ sololearn_javascript01.js
@@ -74,4 +74,132 @@ for (i = 0; i <= 10; i++) {
 }
 
 
-//
+// Changing Attributes (HTML)
+var arr = document.
+    getElementsByTagName("img");
+for(var x=0; x<arr.length; x++) {
+  arr[x].src = "demo.jpg";
+}
+
+
+// Create Element
+//calling the function in window.onload to make sure the HTML is loaded
+/*
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Page Title</title>
+	</head>
+	<body>
+		<div id="demo">some content</div>
+	</body>
+</html>
+*/
+window.onload = function() {
+    //creating a new paragraph
+    var p = document.createElement("p");
+    var node = document.createTextNode("Some new text");
+    //adding the text to the paragraph
+    p.appendChild(node);
+
+    var div = document.getElementById("demo");
+    //adding the paragraph to the div
+    div.appendChild(p);
+};
+
+
+// Remove Element
+//calling the function in window.onload to make sure the HTML is loaded
+/*
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Page Title</title>
+	</head>
+	<body>
+		<div id="demo">
+            <p id="p1">This is a paragraph.</p>
+            <p id="p2">This is another paragraph.</p>
+        </div>
+	</body>
+</html>
+*/
+window.onload = function() {
+    var parent = document.getElementById("demo");
+    var child = document.getElementById("p1");
+    parent.removeChild(child);
+};
+
+
+// Replace Element
+//calling the function in window.onload to make sure the HTML is loaded
+/*
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Page Title</title>
+	</head>
+	<body>
+		<div id="demo">
+            <p id="p1">This is a paragraph.</p>
+            <p id="p2">This is another paragraph.</p>
+        </div>
+	</body>
+</html>
+*/
+window.onload = function() {
+    var p = document.createElement("p");
+    var node = document.createTextNode("This is new");
+    p.appendChild(node);
+
+    var parent = document.getElementById("demo");
+    var child = document.getElementById("p1");
+    parent.replaceChild(p, child);
+};
+
+
+// Amimation
+/*
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Page Title</title>
+	</head>
+	<body>
+		<div id="container">
+            <div id="box"> </div>
+        </div>
+	</body>
+</html>
+*/
+/*
+#container {
+    width: 200px;
+    height: 200px;
+    background: green;
+    position: relative;
+}
+#box {
+    width: 50px;
+    height: 50px;
+    background: red;
+    position: absolute;
+}
+*/
+//calling the function in window.onload to make sure the HTML is loaded
+window.onload = function() {
+     var pos = 0; 
+    //our box element
+    var box = document.getElementById('box');
+    var t = setInterval(move, 10);
+  
+    function move() {
+        if(pos >= 150) {
+            clearInterval(t);
+        }
+        else {
+            pos += 1;
+            box.style.left = pos+'px';
+        }
+    }
+};
